4e4f52a92b7c87393185a4d54e3be3332709c6ee
Xiao Ma <x@medium.com>
2014-09-04 22:51:22 -0700

    Check the database_dir flag and make sure the directory exists

--- bin/launch_local_dynamo.js
+++ bin/launch_local_dynamo.js
@@ -5,11 +5,22 @@
  */
 
 var flags = require('flags')
+var fs = require('fs')
 var localDynamo = require('../lib/launch.js')
 
 flags.defineString('database_dir', '', 'The locaction for databases files')
 flags.defineNumber('port', 4567, 'A port to run DynamoDB Local')
 flags.parse()
+
+if (!flags.get('database_dir')) {
+  console.error('Missing database directory')
+  process.exit(1)
+}
+
+if (!fs.existsSync(flags.get('database_dir'))) {
+  console.error('Database directory does not exist')
+  process.exit(1)
+}
 
 var childProcess = localDynamo.launch(flags.get('database_dir'), flags.get('port'))
 childProcess.stdout.pipe(process.stdout)
