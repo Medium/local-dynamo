c6a92aa6741c142d9f315ce17ae9c16f19fee389
Adrian Lee <adrianlee44@gmail.com>
2016-07-26 00:36:29 -0700

    Add test for sharedDb

--- test/launch_test.js
+++ test/launch_test.js
@@ -32,3 +32,34 @@ exports.testMemory = function (test) {
     test.done()
   }, 5000).unref()
 }
+
+exports.testSharedDb = function (test) {
+  var dynamo = localDynamo.launch({
+    port: 8676,
+    sharedDb: true,
+    stdio: 'pipe'
+  })
+  dynamo.stdout.on('data', function (data) {
+    console.log('stdout', data.toString())
+  })
+  dynamo.stderr.on('data', function (data) {
+    console.log('stderr', data.toString())
+  })
+
+  var finished = false
+
+  dynamo.on('exit', function (code) {
+    if (finished) return
+
+    finished = true
+    test.ok(false, 'Unexpected exit code ' + code)
+    test.done()
+  })
+
+  // If everything goes well after 5 seconds, then we're done!
+  setTimeout(function () {
+    finished = true
+    dynamo.kill()
+    test.done()
+  }, 5000).unref()
+}
