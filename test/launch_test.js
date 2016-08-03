d5d2ffe1c54c5dcb7cdb3c0bd091196c9d61e136
Adrian Lee <adrianlee44@gmail.com>
2016-08-02 18:38:56 -0700

    Add test for CORS support

--- test/launch_test.js
+++ test/launch_test.js
@@ -63,3 +63,35 @@ exports.testSharedDb = function (test) {
     test.done()
   }, 5000).unref()
 }
+
+
+exports.testCors = function (test) {
+  var dynamo = localDynamo.launch({
+    port: 8676,
+    cors: 'medium.com',
+    stdio: 'pipe'
+  })
+  dynamo.stdout.on('data', function (data) {
+    console.log('stdout', data.toString())
+  })
+  dynamo.stderr.on('data', function (data) {
+    console.log('stderr', data.toString())
+  })
+
+  var finished = false
+
+  dynamo.on('exit', function (code) {
+    if (finished) return
+
+    finished = true
+    test.ok(false, 'Unexpected exit code ' + code)
+    test.done()
+  })
+
+  // If everything goes well after 5 seconds, then we're done!
+  setTimeout(function () {
+    finished = true
+    dynamo.kill()
+    test.done()
+  }, 5000).unref()
+}
